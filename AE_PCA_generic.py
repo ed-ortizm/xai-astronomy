385fab362424570d33bfb8ae5f1532ff82f15ae0
Edgar Ortiz <ed.ortizm@gmail.com>
2020-05-11 14:38:25 -0400

    PCs are orthogonal and normalized :)

--- AE_PCA_generic.py
+++ AE_PCA_generic.py
@@ -12,7 +12,7 @@ data[:,0]= np.cos(angles) + np.sin(angles)/2 + noise*np.random.randn(m)/2
 data[:,1]= np.sin(angles)*0.7 + noise* np.random.randn(m)/2
 data[:,2]= data[:,0]*w1 + data[:,1]* w2 + noise*np.random.randn(m)
 
-plot_3D(data)
+#plot_3D(data)
 scaler= StandardScaler()
 
 ## PCA
@@ -22,18 +22,25 @@ pca= PCA(n_components=2)
 pca.fit(X)
 sing_vals = pca.transform(X)
 print(sing_vals.shape)
-plot_2D(sing_vals)
+print('The two principal components are: \n',pca.components_)
+# Let's check if the two principal comoments are orthogonal
+z1 = pca.components_[0]
+z2 = pca.components_[1]
+z12 = z1*z2
+print('The dot product of the two compoments is: ',z12.sum())
+print(np.sum(z1*z1))
+
+#plot_2D(sing_vals)
 #################
-
-
-encoder = keras.models.Sequential([keras.layers.Dense(2, input_shape=[3])])
-decoder = keras.models.Sequential([keras.layers.Dense(3, input_shape=[2])])
-autoencoder = keras.models.Sequential([encoder, decoder])
-
-autoencoder.compile(loss="mse")#, optimizer=keras.optimizers.SGD(lr=0.1))
-history = autoencoder.fit(X, X, epochs=20)
-latent = encoder.predict(X)
-
-print(latent.shape)
-plot_2D(latent)
-plot_2D(np.array([latent[:,1],latent[:,0]]).T)
+#
+# encoder = keras.models.Sequential([keras.layers.Dense(2, input_shape=[3])])
+# decoder = keras.models.Sequential([keras.layers.Dense(3, input_shape=[2])])
+# autoencoder = keras.models.Sequential([encoder, decoder])
+#
+# autoencoder.compile(loss="mse")#, optimizer=keras.optimizers.SGD(lr=0.1))
+# history = autoencoder.fit(X, X, epochs=20)
+# latent = encoder.predict(X)
+#
+# print(latent.shape)
+# plot_2D(latent)
+# plot_2D(np.array([latent[:,1],latent[:,0]]).T)
