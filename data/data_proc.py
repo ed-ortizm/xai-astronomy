75c9b0d5d05c94c7e61089a1af79122963db5632
Edgar Ortiz <ed.ortizm@gmail.com>
2020-05-12 17:25:13 -0400

    non-galaxies removed. Galaxies table created

--- data/data_proc.py
+++ data/data_proc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import numpy as np
 from astropy.table import Table
 
@@ -8,4 +9,16 @@ data= Table.read(file)
 
 # Selecting only galaxies
 gal_mask= data['objtype']=='GALAXY'
-galaxies = data[gal_mask]
+galaxies= data[gal_mask]
+galaxies.write('gals-spObj-0266-51630-23.fit', format='fits')
+
+
+# Removing non-galaxies from directory
+head = 'spSpec/spSpec-51630-0266-'
+for object in data:
+    if object['objtype'] != 'GALAXY':
+        id= object['fiberid']
+        file= f'{head}{id:03}.fit'
+        if os.path.exists(file):
+            os.remove(file)
+        else: print(file, 'file does not exist!')
