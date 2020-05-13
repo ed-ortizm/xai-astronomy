ed7f4ca6f1cf96f6876443b4cfd87d409c55efc9
Edgar Ortiz <ed.ortizm@gmail.com>
2020-05-13 16:53:11 -0400

    data saved in a n_f x n_2 x n_pixels array

--- data/data_proc.py
+++ data/data_proc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from data_proc_lib import *
+t_i= time.time()
 # Loading table with info of the data
 file= 'spObj-0266-51630-23.fit'
 data= Table.read(file)
@@ -29,31 +30,42 @@ for galaxy in galaxies:
     id= galaxy['fiberid']
     file= f'{head}{id:03}.fit'
     files.append(file)
-print(len(files))
 
-# Obtaining the redshift
-print(files[0])
-hdul = fits.open(dir+files[0])
-Z = hdul[0].header['Z']
+n_pixels= 3834
+data= np.zeros((len(files),2,n_pixels))
+idx= 0
+for file in files:
+    # Obtaining the redshift
+    hdul = fits.open(dir+file)
+    Z = hdul[0].header['Z']
 
-# Wavelengths
+    # Wavelengths divided by redshift
 
-COEFF0 = hdul[0].header['COEFF0']
-COEFF1 = hdul[0].header['COEFF1']
+    COEFF0 = hdul[0].header['COEFF0']
+    COEFF1 = hdul[0].header['COEFF1']
 
-wavelengths= []
-n_pixels= hdul[0].header['NAXIS1']
-for i in range(n_pixels):
-    lbd= 10**(COEFF0+COEFF1*i)
-    wavelengths.append(lbd)
+    wavelengths= []
 
-print(wavelengths[0],wavelengths[-1])
+    for i in range(n_pixels):
+        lbd= 10**(COEFF0+COEFF1*i)
+        wavelengths.append(lbd)
+    wavelengths=np.array(wavelengths)/Z
 
-# Mean value of the flux
+    # Mean value of the flux
+    # First row is the spectrum
 
-data = hdul[0].data
-median= np.median(data)
-data = data-median
+    flux = hdul[0].data[0]
+    hdul.close()
 
+    median= np.median(flux)
+    flux = flux-median
+    print(idx)
+    data[idx]= np.array([wavelengths,flux])
+    idx= idx+1
 
-hdul.close()
+# plt.figure()
+# plt.plot(wavelengths,data)
+# plt.show()
+# plt.close()
+t_f= time.time()
+print(t_f-t_i)
--- data/data_proc_lib.py
+++ data/data_proc_lib.py
@@ -2,6 +2,9 @@ import os
 import numpy as np
 from astropy.table import Table
 from astropy.io import fits
+import matplotlib
+import matplotlib.pyplot as plt
+import time
 from multiprocessing import Pool
 
 class Data:
