dd75c2e392792e802425714933970ba788838c66
Edgar Ortiz <ed.ortizm@gmail.com>
2020-05-26 11:42:14 -0400

    Number of files with SPEC_CLN=2 counted. There are 509

--- data/data_proc.py
+++ data/data_proc.py
@@ -4,47 +4,65 @@ from astropy.table import Table
 import numpy as np
 from astropy.io import fits
 #t_i= time.time()
-# Loading table with info of the data
 ## count the number of files with SPEC_CLN=2 in all files
 # and in files with objtype='GALAXY'. If they are different
 # Do a catalog with the ones that are both 'GALAXY' and SPEC_CLN=2
 # Set apart the ones that are 'GALAXY' but not SPEC_CLN=2
 # Set apart the ones  that are SPEC_CLN=2 but not 'GALAXY'
+
+# Loading table with info of the data
 fname = 'spObj-0266-51630-23.fit'
 data = Table.read(fname)
 
-# Selecting only galaxies
-non_galaxies = data[np.where(data['objtype'] != 'GALAXY')]
-non_galaxies.write('non-gals-spObj-0266-51630-23.fit', format='fits',
-                   overwrite=True)
-
-dir = 'spSpec/'
+# Number of files with SPEC_CLN=2
+dir = 'spSpec_org/'
 head = 'spSpec-51630-0266-'
-for fiber_id in non_galaxies['fiberid']:
-    fname = f'{dir}{head}{fiber_id:03}.fit'
-    if os.path.exists(fname):
-        os.remove(fname)
-    else:
-        print(f'{fname} does not exist!')
 
-# List of files
-galaxies = data[np.where(data['objtype'] == 'GALAXY')]
 fnames = []
 
-for fiber_id in galaxies['fiberid']:
-    fname = f'{head}{fiber_id:03}.fit'
+for fiber_id in data['fiberid']:
+    fname = f'{dir}{head}{fiber_id:03}.fit'
     fnames.append(fname)
-print(f'There are {len(fnames)} galaxies')
 
-# Wavelength range: the two coeficients neccessary to compute
-# the wavelength range are the same among all the .fit files
+gal_count = 0
+for fname in fnames:
+    with fits.open(fname) as hdul:
+        if hdul[0].header['SPEC_CLN']==2:
+            gal_count += 1
 
-with fits.open(dir+fnames[0]) as hdul:
-    n_pixels = hdul[0].header['NAXIS1']
-    COEFF0 = hdul[0].header['COEFF0']
-    COEFF1 = hdul[0].header['COEFF1']
-
-wl_range = 10. ** (COEFF0 + COEFF1 * np.arange(n_pixels))
+print(gal_count)
+# There are 509 files with SPEC_CLN=2
+# # Selecting only galaxies
+# non_galaxies = data[np.where(data['objtype'] != 'GALAXY')]
+# non_galaxies.write('non-gals-spObj-0266-51630-23.fit', format='fits',
+#                    overwrite=True)
+#
+# dir = 'spSpec/'
+# for fiber_id in non_galaxies['fiberid']:
+#     fname = f'{dir}{head}{fiber_id:03}.fit'
+#     if os.path.exists(fname):
+#         os.remove(fname)
+#     else:
+#         print(f'{fname} does not exist!')
+#
+# # List of files
+# galaxies = data[np.where(data['objtype'] == 'GALAXY')]
+# fnames = []
+#
+# for fiber_id in galaxies['fiberid']:
+#     fname = f'{head}{fiber_id:03}.fit'
+#     fnames.append(fname)
+# print(f'There are {len(fnames)} galaxies')
+#
+# # Wavelength range: the two coeficients neccessary to compute
+# # the wavelength range are the same among all the .fit files
+#
+# with fits.open(dir+fnames[0]) as hdul:
+#     n_pixels = hdul[0].header['NAXIS1']
+#     COEFF0 = hdul[0].header['COEFF0']
+#     COEFF1 = hdul[0].header['COEFF1']
+#
+# wl_range = 10. ** (COEFF0 + COEFF1 * np.arange(n_pixels))
 
 # double check if the vacumn ws are the same.... if so, do (w)/(1+Z)
 # where Z is an array (broadcasting) double check if you can take then
