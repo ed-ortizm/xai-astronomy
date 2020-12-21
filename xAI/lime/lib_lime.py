578671d58aeb130411c7f691c82acb54ca192881
Edgar Ortiz <ed.ortiz@gmail.com>
2020-12-21 12:25:47 -0300

    Parallelization implemented to compute the explainer and explanations

--- xAI/lime/lib_lime.py
+++ xAI/lime/lib_lime.py
@@ -1,4 +1,39 @@
+import csv
+from joblib import Parallel, delayed
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import multiprocessing
+import numpy as np
 import lime
 import lime.lime_tabular
-import matplotlib.pyplot as plt
-import numpy as np
+from tensorflow.keras.models import load_model
+
+def explain(kernel_width, training_data, training_labels, data_row,
+                predict_fn, num_features, file):
+
+    print(f'Explainer with kernel width: {kernel_width}')
+
+    explainer = lime.lime_tabular.LimeTabularExplainer(
+    training_data=training_data, mode='regression',
+    raining_labels = training_labels, kernel_width=kernel_width, verbose=True)
+
+    print(f'Computing explanations for the top MSE outlier')
+    exp = explainer.explain_instance(data_row=data_row, predict_fn=predict_fn,
+    num_features=num_features)
+
+    print(f'Saving explanation as html')
+    exp.save_to_file(file_path=f'./kw_{kernel_width}_explanation_AE_p.html')
+
+    # explanation as list
+    print(f'Explanation list to a file...')
+    exp_list = exp.as_list()
+    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+    wr.writerow(exp_list)
+
+    # explanation as pyplot figure
+    print(f'Explanation to pyplot figure')
+    exp_fig = exp.as_pyplot_figure()
+    exp_fig.savefig(f'./kw_{kernel_width}_explanation_AE_p.png')
+
+    return exp
--- xAI/lime/lime_spectra.py
+++ xAI/lime/lime_spectra.py
@@ -3,11 +3,17 @@
 import time
 
 import csv
+from joblib import Parallel, delayed
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import multiprocessing
 import numpy as np
 import lime
 import lime.lime_tabular
 from tensorflow.keras.models import load_model
+
+from lib_lime import explain
 
 ti = time.time()
 ################################################################################
@@ -27,37 +33,53 @@ pred = AE.predict(spec)
 # Selecting top outliers for explanations
 mse_score = np.square(pred-spec).mean(axis=1)
 max_id = np.argmax(mse_score)
-print(max_id)
 
-# Creating explainers
+print(f'The id for the top MSE outlier is {max_id}')
 
-list_file = open('explanations_list.csv', 'w', newline='\n')
 k_widths = [5, 10, 20, 38, 50, 75, 100, 250, 500, 1_000]
-for kernel_width in k_widths:
 
-    print(f'Creating explainer...')
-    explainer = lime.lime_tabular.LimeTabularExplainer(training_data=spec,
-                mode='regression', training_labels = spec,
-                kernel_width=kernel_width, verbose=True)
+n_cores = multiprocessing.cpu_count()
 
-    # Generating an explanation
-    print(f'Generating explanation...')
-    exp = explainer.explain_instance(spec[max_id, :], AE.predict,
-          num_features=100)
+if __name__ == "__main__":
 
-    print(f'Saving explanation as html')
-    exp.save_to_file(file_path=f'./kw_{kernel_width}_explanation_AE.html')
+# Parallelization
 
-    # explanation as list
-    exp_list = exp.as_list()
-    wr = csv.writer(list_file, quoting=csv.QUOTE_ALL)
-    wr.writerow(exp_list)
-    
-    # explanation as pyplot figure
-    exp_fig = exp.as_pyplot_figure()
-    exp_fig.savefig(f'./kw_{kernel_width}_explanation_AE.png')
+    explanations_csv = open('explanations_list_p.csv', 'w', newline='\n')
 
-list_file.close()
+    explainers_list = Parallel(n_jobs=n_cores)(delayed(explain)
+    (kernel_width=kernel_width, training_data=spec, training_labels=spec,
+    data_row = spec[max_id, :], predict_fn=AE.predict, num_features=100,
+    file = explanations_csv) for kernel_width in k_widths)
+
+    explanations_csv.close()
+
+# for kernel_width in k_widths:
+#
+#     print(f'Creating explainer...')
+#     explainer = lime.lime_tabular.LimeTabularExplainer(training_data=spec,
+#                 mode='regression', training_labels = spec,
+#                 kernel_width=kernel_width, verbose=True)
+#
+#     # Generating an explanation
+#     print(f'Generating explanation...')
+#     exp = explainer.explain_instance(spec[max_id, :], AE.predict,
+#           num_features=100)
+#
+#     print(f'Saving explanation as html')
+#     exp.save_to_file(file_path=f'./kw_{kernel_width}_explanation_AE.html')
+#
+#     # explanation as list
+#     exp_list = exp.as_list()
+#     wr = csv.writer(list_file, quoting=csv.QUOTE_ALL)
+#     wr.writerow(exp_list)
+#
+#     # explanation as pyplot figure
+#     exp_fig = exp.as_pyplot_figure()
+#     exp_fig.savefig(f'./kw_{kernel_width}_explanation_AE.png')
+#
+# list_file.close()
+
+
 # I must use the trained model to add a layer that computes the outlier
 # score and that would be a new model which I want to explain
 # outlier_exp = explainer.explain_instance(spec[0, :], np,
