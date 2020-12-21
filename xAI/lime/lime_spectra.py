988b9b2cc51aa1fd6b4c50447e1cfe201dc22501
Edgar Ortiz <ed.ortiz@gmail.com>
2020-12-21 10:41:15 -0300

    Testing different kernel widths

--- xAI/lime/lime_spectra.py
+++ xAI/lime/lime_spectra.py
@@ -2,6 +2,7 @@
 
 import time
 
+import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import lime
@@ -27,21 +28,36 @@ pred = AE.predict(spec)
 mse_score = np.square(pred-spec).mean(axis=1)
 max_id = np.argmax(mse_score)
 print(max_id)
-# Creating explainer
 
-kernel_width = 20
-print(f'Creating explainer...')
-explainer = lime.lime_tabular.LimeTabularExplainer(training_data=spec,
-            mode='regression', training_labels = spec,
-            kernel_width=kernel_width, verbose=True)
+# Creating explainers
 
-# Generating an explanation
-print(f'Generating explanation...')
-exp = explainer.explain_instance(spec[max_id, :], AE.predict,
-      num_features=int(spec.shape[1]/20))
+list_file = open('explanations_list.csv', 'w', newline='\n')
+k_widths = [5, 10, 20, 38, 50, 75, 100, 250, 500, 1_000]
+for kernel_width in k_widths:
 
-print(f'Saving explanation as html')
-exp.save_to_file(file_path='./explanation_AE.html')
+    print(f'Creating explainer...')
+    explainer = lime.lime_tabular.LimeTabularExplainer(training_data=spec,
+                mode='regression', training_labels = spec,
+                kernel_width=kernel_width, verbose=True)
+
+    # Generating an explanation
+    print(f'Generating explanation...')
+    exp = explainer.explain_instance(spec[max_id, :], AE.predict,
+          num_features=100)
+
+    print(f'Saving explanation as html')
+    exp.save_to_file(file_path=f'./kw_{kernel_width}_explanation_AE.html')
+
+    # explanation as list
+    exp_list = exp.as_list()
+    wr = csv.writer(list_file, quoting=csv.QUOTE_ALL)
+    wr.writerow(exp_list)
+    
+    # explanation as pyplot figure
+    exp_fig = exp.as_pyplot_figure()
+    exp_fig.savefig(f'./kw_{kernel_width}_explanation_AE.png')
+
+list_file.close()
 # I must use the trained model to add a layer that computes the outlier
 # score and that would be a new model which I want to explain
 # outlier_exp = explainer.explain_instance(spec[0, :], np,
