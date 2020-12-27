5410130d5c5afab3051d99df8fb4c794d7cc26a0
Edgar Ortiz <ed.ortiz@gmail.com>
2020-12-27 15:53:56 -0300

    Modifying script to process the explanation list. Check the fact that now a file es a kernel having explanation for n_outliers. Transform it to .npy for each outlier with different kernels

--- xAI/lime/proc_exp_lime.py
+++ xAI/lime/proc_exp_lime.py
@@ -1,60 +1,84 @@
 #! /usr/bin/env python3
 
 import time
+import glob
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 ti = time.time()
 ################################################################################
-k_widths = [5, 10, 20, 38, 50, 75, 100, 250, 500, 1_000]
-explanations = {}
+# Extracting explanation files paths and kernel widths
 
-with open('explanations_list.csv', newline='\n') as file:
+print(f'Extracting explanation files paths and kernel widths...')
+lime_path ='/home/edgar/zorro/outlier_AEs/xAI/lime'
+exp_files = glob.glob(f'{lime_path}/*_kernel/*.csv')
 
-    for idx, line in enumerate(file):
-        explanations[f'kernel width = {k_widths[idx]}'] = line
+k_widths = []
+for fname in exp_files:
 
-for kernel_key in explanations:
+    width = fname.split('/')[-1].split('_')[0]
+    k_widths.append(int(width))
 
-    exp = explanations[kernel_key]
-    exp = exp.split('"')
-    exp = list(dict.fromkeys(exp))
-    exp.remove(',')
-    exp.remove('')
-    exp.remove('\r\n')
+# Preparing explanations
+print(len(exp_files))
+for fname in exp_files:
 
-    explanations[kernel_key] = exp
+    explanations = {}
 
-feature_weight = np.empty((10, 100, 3)) # 10 kernels, 100 explained features,
-# feature and value --> (10, 100, 2)
+    with open(f'{fname}', newline='\n') as file:
 
-for kernel_idx, kernel_key in enumerate(explanations):
+        for idx, line in enumerate(file):
+            explanations[f'{idx}_outlier'] = line
 
-    exp: explanations[kernel_key]
-    for feature_idx, tuple in enumerate(exp):
+    for outlier_key in explanations:
 
-        tuple = tuple.split(',')
+        exp = explanations[outlier_key]
+        exp = exp.split('"')
+        exp = list(dict.fromkeys(exp))
+        exp.remove(',')
+        exp.remove('')
+        exp.remove('\r\n')
 
-        tuple[0] = tuple[0][2:-1]
-        tuple[1] = np.float(tuple[1][:-1])
+        explanations[outlier_key] = exp
 
-        if '<' in tuple[0]:
 
-            if len(tuple[0].split('<'))==2:
-                tuple[0] = np.int(tuple[0].split('<')[0])
+    n_outliers = len(explanations)
+    n_features = len(exp)
+    n_values = len(exp[0].split(','))
+    # print(n_outliers, n_features, n_values)
+    feature_weight = np.empty((n_outliers, n_features, n_values+1))
+    # n_values + 1 to add the outlier identifier
+
+    for outlier_idx, outlier_key in enumerate(explanations):
+
+        exp = explanations[outlier_key]
+        for feature_idx, tuple in enumerate(exp):
+
+            tuple = tuple.split(',')
+
+            tuple[0] = tuple[0][2:-1]
+            tuple[1] = np.float(tuple[1][:-1])
+
+            if '<' in tuple[0]:
+
+                if len(tuple[0].split('<'))==2:
+                    tuple[0] = np.int(tuple[0].split('<')[0])
+                else:
+                    tuple[0] = np.int(tuple[0].split('<')[1])
+
             else:
-                tuple[0] = np.int(tuple[0].split('<')[1])
+                # print(tuple[0].split('>')[0], tuple[0].split('>')[1])
+                tuple[0] = np.int(tuple[0].split('>')[0])
 
-        else:
-            # print(tuple[0].split('>')[0], tuple[0].split('>')[1])
-            tuple[0] = np.int(tuple[0].split('>')[0])
+            print(feature_idx, tuple)
 
-        tuple.append(k_widths[kernel_idx])
-        feature_weight[kernel_idx, feature_idx, :] = np.array(tuple)
+            break
+            tuple.append(outlier_idx)
+            feature_weight[outlier_idx, feature_idx, :] = np.array(tuple)
 
-print(feature_weight[0, 0, :])
-np.save('features_exp_weights_10_kwidths.npy', feature_weight)
+    # print(feature_weight[1, 0, 1])
+    # np.save('features_exp_weight.npy', feature_weight)
 
 ################################################################################
 tf = time.time()
