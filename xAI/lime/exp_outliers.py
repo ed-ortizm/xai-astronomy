284c80639991779112108880354797843416a5bd
Edgar Ortiz <ed.ortiz@gmail.com>
2020-12-28 13:42:59 -0300

    array having all outliers and explanatios with the following format [n_outlier, n_kernel, n_feature, exp_weight]

--- xAI/lime/exp_outliers.py
+++ xAI/lime/exp_outliers.py
@@ -27,10 +27,38 @@ for idx, fname in enumerate(outlier_files):
 
     spectra[idx, :] = np.load(fname)
 
-# The explanation
+# The explanations
 exp_files = glob.glob(f'{data_path}/outlier_feature_weight_k_size_*.npy')
 
-# for fname in exp_files:
+print(f'Loading explanations for all kernels: [n_outlier, feature, weight]')
+
+n_kernels = len(exp_files)
+tmp = np.load(exp_files[0])
+n_outliers = tmp.shape[0]
+n_features = tmp.shape[1]
+n_values = tmp.shape[2] + 1 # +1 to include the kernel value
+
+tmp = np.empty((n_outliers, n_kernels, n_features, n_values))
+# outlier_exps = {f'{int(n)}' : np.empty((n_kernels, n_features, n_values))
+# for n in tmp[:, 0, 0]}
+
+for idx_kernel, fname in enumerate(exp_files):
+
+    n_kernel = float(fname.split('_')[-1].split('.')[0])
+    tmp[:, idx_kernel, :, 1] = n_kernel
+
+    exps = np.load(fname)
+    # print(n_kernel)
+    for idx_outlier, exp in enumerate(exps):
+
+        # exp = np.hstack(exp[],np.array([n_kernel]), )
+        tmp[idx_outlier, idx_kernel, :, [0, 2, 3]] = exp[:, :].T
+        # tmp[idx_outlier, idx_kernel, :, 2:] = exp[:, 1:]
+
+        break
+    for row in tmp[0, idx_kernel, 0, :]:
+        print(row)
+    break
 
 ################################################################################
 tf = time.time()
