969707db8d3600d2598ce002c7394c0be815083d
Edgar Ortiz <ed.ortiz@gmail.com>
2021-01-02 09:47:53 -0300

    Saving results where all features were taken as explainable features

--- xAI/lime/p_lime_spectra.py
+++ xAI/lime/p_lime_spectra.py
@@ -21,16 +21,16 @@ ti = time.time()
 k_n = int(sys.argv[1])
 # Data used to train the model
 train_data_path = '/home/edgar/zorro/SDSSdata/SDSS_data_curation/spec_99356.npy'
+outlier_score_path = '/home/edgar/zorro/outlier_AEs/xAI/lime/spectra_scores'
 spec = np.load(f'{train_data_path}')
 
 # # Selecting top outliers for explanations
 o_score_mse = mse_score(O=spec)
 
 # From visual exploration, check projec_gal_retireve for metadata of spectra
-outliers_mse = np.load('outliers_mse.npy')
+outliers_mse = np.load(f'{outlier_score_path}/outliers_mse.npy')
 to_explain_spec_idx = [outliers_mse[i] for i in [24, 28, 23, 21, 20, 19, 18, 17, 16]]
 outliers_to_exp = [spec[i, :] for i in to_explain_spec_idx]
-
 
 k_widths = [5, 10, 20, 38, 50, 75, 100]
 
@@ -40,10 +40,12 @@ explainer = lime.lime_tabular.LimeTabularExplainer(training_data=spec,
             mode='regression', training_labels = o_score_mse,
             kernel_width=k_widths[k_n], verbose=True)
 
-explanations_csv = open(
-f'{k_widths[k_n]}_kernel/{k_widths[k_n]}_k_outlier_exp_AE.csv', 'w', newline='\n')
-
 # Generating an explanation
+num_features = spec.shape[1]
+explanations_csv = open(
+f'{k_widths[k_n]}_kernel/{k_widths[k_n]}_k_outlier_nfeat_{num_features}_exp_AE.csv', 'w', newline='\n')
+
+
 for j, outlier in enumerate(outliers_to_exp):
 
     np.save(f'{j}_outlier.npy', outlier)
@@ -51,11 +53,12 @@ for j, outlier in enumerate(outliers_to_exp):
 
     print(f'Generating explanation...')
     exp = explainer.explain_instance(outlier, mse_score,
-          num_features=100)
+          num_features=num_features)
 
     print(f'Saving explanation as html')
     exp.save_to_file(
-    file_path=f'{k_widths[k_n]}_kernel/{j}_outlier_k_{k_widths[k_n]}_exp_AE.html')
+    file_path=\
+    f'{k_widths[k_n]}_kernel/{j}_outlier_k_{k_widths[k_n]}_nfeat_{num_features}_exp_AE.html')
 
 
     # explanation as list
@@ -65,7 +68,8 @@ for j, outlier in enumerate(outliers_to_exp):
 
     # explanation as pyplot figure
     exp_fig = exp.as_pyplot_figure()
-    exp_fig.savefig(f'{k_widths[k_n]}_kernel/{j}_outlier_k_{k_widths[k_n]}_exp_AE.png')
+    exp_fig.savefig(
+    f'{k_widths[k_n]}_kernel/{j}_outlier_k_{k_widths[k_n]}_nfeat_{num_features}_exp_AE.png')
 
 explanations_csv.close()
 ################################################################################
