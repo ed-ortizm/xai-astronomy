1a51ebaee760284dd09b2d61476a7bfa93fe9365
Edgar Ortiz <ed.ortizm@gmail.com>
2021-04-30 06:02:07 -0400

    Updating outlier metrics

--- library_outlier.py
+++ library_outlier.py
@@ -7,89 +7,148 @@ class Outlier:
     tensorflow.keras
     """
     ############################################################################
-    def __init__(self, metric:'str', model:'tf.keras.model'):
-        """
-        Init fucntion
-
-        Args:
-
-            metric: (str) the name of the metric used to compute the outlier score
-                using the observed spectrum and its reconstruction. Possible
-
-            p: (float > 0) in case the metric is the lp metric, p needs to be a
-                non null possitive float [Aggarwal 2001]
-        """
+    def __init__(self, metric:'str', model:'tf.keras.model',
+        p:'float'=0.25, epsilon=1E-3):
 
         self.metric = metric
         self.model = model
-        # if metric=='lp' and p>0:
-        #     self.p = p
-        # if:
-        #     print(f'For lp metric p must be positive, instead p={p}')
-        #     print(f'Failed to instantiate class')
-        #     sys.exit()
+        self.p = p
+        self.epsilon = epsilon
     ############################################################################
     def score(self, O, percentage, image):
-        """
-        Computes the outlier score according to the metric used to instantiate
-        the class.
 
-        Args:
-            O: (2D np.array) with the original objects where index 0 indicates
-            the object and index 1 the features of the object.
+        print(f'Computing {self.metric} scores')
 
-            R: (2D np.array) with the reconstructed objects where index 0
-            indicates the object and index 1 the features of the object.
-
-        Returns:
-            A one dimensional numpy array with the outlier scores for objects
-            present in O
-        """
         if self.metric == 'mse':
-            print(f'Computing the outlier scores')
 
             R = self.model.predict(O)
-            print(f'O: {O.shape}    R:{R.shape}')
-            score = self._mse(O=O, R=R,
-                percentage=percentage, image=image)
+            return self._mse(O=O, R=R, percentage=percentage, image)
 
-            return score
+        elif self.metric == 'mse_relative':
+            R = self.model.predict(O)
+            return self._mse_relative(O=O, R=R, percentage=percentage, image)
+
+        elif self.metric == 'mad':
+            R = self.model.predict(O)
+            return self._mad(O=O, R=R, percentage=percentage, image)
+
+        elif self.metric == 'mad_relative':
+            R = self.model.predict(O)
+            return self._mad_relative(O=O, R=R, percentage=percentage, image)
+
+        elif self.metric == 'lp':
+            R = self.model.predict(O)
+            return self._lp(O=O, R=R, percentage=percentage, image)
+
+        elif self.metric == 'lp_relative':
+            R = self.model.predict(O)
+            return self._lp_relative(O=O, R=R, percentage=percentage, image)
 
         else:
             print(f'The provided metric: {self.metric} is not implemented yet')
             sys.exit()
+        # print(f'Computing the outlier scores')
+        #
+        # if self.metric == 'mse':
+        #
+        #     R = self.model.predict(O)
+        #
+        #     score = self._mse(O=O, R=R,
+        #         percentage=percentage, image=image)
+        #
+        #     return score
+        #
+        # else:
+        #     print(f'The provided metric: {self.metric} is not implemented yet')
+        #     sys.exit()
+    ############################################################################
     ############################################################################
     def _mse(self, O, R, percentage, image):
-        """
-        Computes the mean square error for the reconstruction of the input
-        objects
-
-        Args:
-            O: (2D np.array) with the original objects where index 0 denotes
-                indicates the objec and index 1 the features of the object.
-
-            R: Reconstruction of O by (tensorflow.keras model) the generative
-            model
-
-        Returns:
-            A one dimensional numpy array with the mean square error for objects
-            present in O
-        """
 
         if image:
             O, R = O[:, 0, :, 0], R[:, 0, :, 0]
 
-        mse = np.abs(R - O)/np.abs(O+0.0001)
+        reconstruction = np.square(R - O)
 
-        number_outlier_fluxes = int(percentage*mse.shape[1])
-        highest_mse = np.argpartition(mse, -1 * number_outlier_fluxes,
+        number_outlier_fluxes = int(0.01*percentage*reconstruction.shape[1])
+
+        largest_reconstruction_ids = np.argpartition(
+            reconstruction,
+            -1 * number_outlier_fluxes,
             axis=1)[:, -1 * number_outlier_fluxes:]
 
-        score = np.empty(highest_mse.shape)
+        score = np.empty(largest_reconstruction_ids.shape)
 
-        for n, idx in enumerate(highest_mse):
+        for n, idx in enumerate(largest_reconstruction_ids):
 
-            score[n, :] = mse[n, idx]
+            score[n, :] = reconstruction[n, idx]
+
+        o_score = score.sum(axis=1)
+        if image:
+
+            similarity =  o_score.max() - o_score
+
+            o_similarity = np.empty((o_score.size, 2))
+            o_similarity[:, 0] = o_score[:]
+            o_similarity[:, 1] = similarity[:]
+
+            return o_similarity
+
+        return o_score
+    ############################################################################
+    def _mse_relative(self, O, R, percentage, image):
+
+        if image:
+            O, R = O[:, 0, :, 0], R[:, 0, :, 0]
+
+        reconstruction = np.square( (R - O)/(O + self.epsilon) )
+
+        number_outlier_fluxes = int(0.01*percentage*reconstruction.shape[1])
+
+        largest_reconstruction_ids = np.argpartition(
+            reconstruction,
+            -1 * number_outlier_fluxes,
+            axis=1)[:, -1 * number_outlier_fluxes:]
+
+        score = np.empty(largest_reconstruction_ids.shape)
+
+        for n, idx in enumerate(largest_reconstruction_ids):
+
+            score[n, :] = reconstruction[n, idx]
+
+        o_score = score.sum(axis=1)
+        if image:
+
+            similarity =  o_score.max() - o_score
+
+            o_similarity = np.empty((o_score.size, 2))
+            o_similarity[:, 0] = o_score[:]
+            o_similarity[:, 1] = similarity[:]
+
+            return o_similarity
+
+        return o_score
+    ############################################################################
+    ############################################################################
+    def _mad(self, O, R, percentage, image):
+
+        if image:
+            O, R = O[:, 0, :, 0], R[:, 0, :, 0]
+
+        reconstruction = np.abs(R - O)
+
+        number_outlier_fluxes = int(0.01*percentage*reconstruction.shape[1])
+
+        largest_reconstruction_ids = np.argpartition(
+            reconstruction,
+            -1 * number_outlier_fluxes,
+            axis=1)[:, -1 * number_outlier_fluxes:]
+
+        score = np.empty(largest_reconstruction_ids.shape)
+
+        for n, idx in enumerate(largest_reconstruction_ids):
+
+            score[n, :] = reconstruction[n, idx]
 
         o_score = score.sum(axis=1)
 
@@ -104,6 +163,109 @@ class Outlier:
             return o_similarity
 
         return o_score
+    ############################################################################
+    def _mad_relative(self, O, R, percentage, image):
+
+        if image:
+            O, R = O[:, 0, :, 0], R[:, 0, :, 0]
+
+        reconstruction = np.abs( (R - O)/(O + self.epsilon) )
+
+        number_outlier_fluxes = int(0.01*percentage*reconstruction.shape[1])
+
+        largest_reconstruction_ids = np.argpartition(
+            reconstruction,
+            -1 * number_outlier_fluxes,
+            axis=1)[:, -1 * number_outlier_fluxes:]
+
+        score = np.empty(largest_reconstruction_ids.shape)
+
+        for n, idx in enumerate(largest_reconstruction_ids):
+
+            score[n, :] = reconstruction[n, idx]
+
+        o_score = np.sum(score, axis=1)
+        if image:
+
+            similarity =  o_score.max() - o_score
+
+            o_similarity = np.empty((o_score.size, 2))
+            o_similarity[:, 0] = o_score[:]
+            o_similarity[:, 1] = similarity[:]
+
+            return o_similarity
+
+        return o_score
+    ############################################################################
+    ############################################################################
+    def _lp(self, O, R, percentage, image):
+
+        if image:
+            O, R = O[:, 0, :, 0], R[:, 0, :, 0]
+
+        reconstruction = np.abs(R - O)**self.p
+
+        number_outlier_fluxes = int(0.01*percentage*reconstruction.shape[1])
+
+        largest_reconstruction_ids = np.argpartition(
+            reconstruction,
+            -1 * number_outlier_fluxes,
+            axis=1)[:, -1 * number_outlier_fluxes:]
+
+        score = np.empty(largest_reconstruction_ids.shape)
+
+        for n, idx in enumerate(largest_reconstruction_ids):
+
+            score[n, :] = reconstruction[n, idx]
+
+        o_score = np.sum(score, axis=1)**(1 / self.p)
+
+        if image:
+
+            similarity =  o_score.max() - o_score
+
+            o_similarity = np.empty((o_score.size, 2))
+            o_similarity[:, 0] = o_score[:]
+            o_similarity[:, 1] = similarity[:]
+
+            return o_similarity
+
+        return o_score
+    ############################################################################
+    def _lp_relative(self, O, R, percentage, image):
+
+        if image:
+            O, R = O[:, 0, :, 0], R[:, 0, :, 0]
+
+        reconstruction = np.abs( (R - O)/(O + self.epsilon) )**self.p
+
+        number_outlier_fluxes = int(0.01*percentage*reconstruction.shape[1])
+
+        largest_reconstruction_ids = np.argpartition(
+            reconstruction,
+            -1 * number_outlier_fluxes,
+            axis=1)[:, -1 * number_outlier_fluxes:]
+
+        score = np.empty(largest_reconstruction_ids.shape)
+
+        for n, idx in enumerate(largest_reconstruction_ids):
+
+            score[n, :] = reconstruction[n, idx]
+
+        o_score = np.sum(score, axis=1)**(1 / self.p)
+
+        if image:
+
+            similarity =  o_score.max() - o_score
+
+            o_similarity = np.empty((o_score.size, 2))
+            o_similarity[:, 0] = o_score[:]
+            o_similarity[:, 1] = similarity[:]
+
+            return o_similarity
+
+        return o_score
+    ############################################################################
     ############################################################################
     def top_reconstructions(self, scores, n_top_spectra):
         """
@@ -131,3 +293,33 @@ class Outlier:
 
         return most_normal_ids, most_oulying_ids
 ################################################################################
+    # def _mse(self, O, R, percentage, image):
+    #
+    #     if image:
+    #         O, R = O[:, 0, :, 0], R[:, 0, :, 0]
+    #
+    #     mse = np.abs(R - O)/np.abs(O+0.0001)
+    #
+    #     number_outlier_fluxes = int(percentage*mse.shape[1])
+    #     highest_mse = np.argpartition(mse, -1 * number_outlier_fluxes,
+    #         axis=1)[:, -1 * number_outlier_fluxes:]
+    #
+    #     score = np.empty(highest_mse.shape)
+    #
+    #     for n, idx in enumerate(highest_mse):
+    #
+    #         score[n, :] = mse[n, idx]
+    #
+    #     o_score = score.sum(axis=1)
+    #
+    #     if image:
+    #
+    #         similarity =  o_score.max() - o_score
+    #
+    #         o_similarity = np.empty((o_score.size, 2))
+    #         o_similarity[:, 0] = o_score[:]
+    #         o_similarity[:, 1] = similarity[:]
+    #
+    #         return o_similarity
+    #
+    #     return o_score
