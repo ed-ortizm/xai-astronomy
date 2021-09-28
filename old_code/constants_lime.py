008490d40e0b978ea2fdc7800f8bd2884d29694f
Edgar Ortiz <ed.ortizm@gmail.com>
2021-09-28 11:56:25 -0300

    Get rid of old files

--- old_code/constants_lime.py
+++
@@ -1,7 +0,0 @@
-normalization_schemes = ['median', 'min_max', 'Z']
-
-explanation_dir = '/home/edgar/oso/xAI-outlier/explanation_files'
-
-models_dir = '/home/edgar/oso/VAE-outlier/models'
-spectra_dir = '/home/edgar/oso/data_sdss/spectra'
-working_dir = '/home/edgar/oso/xAI-outlier'
