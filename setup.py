ada1ca3bc2cb214b365946dc59040171fb862b33
Edgar Ortiz <ed.ortizm@gmail.com>
2021-10-05 11:19:37 -0300

    Set setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="astroxai",
+    version="0.0.1",
+    author="Edgar Ortiz",
+    author_email="ed.ortizm@gmail.com",
+    packages = find_packages(
+        where='src',
+        include=['[a-z]*'],
+        exclude=[]
+    ),
+    package_dir = {"":"src"},
+    description="Python Code For explainable artificial intelligence in astronomy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ed-ortizm/xai-astronomy",
+    license="MIT",
+    keywords="astrophysics, galaxy, Machine Learning, LIME, SHAP"
+)
