3f88da1096ae7e7f03746753f9ea28c3c4901634
Edgar Ortiz <ed.ortizm@gmail.com>
2022-02-13 11:44:56 -0300

    Update repo package name from astroxai to astroExplain

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as file:
     long_description = file.read()
 
 setup(
-    name="astroxai",
+    name="astroExplain",
     version="0.0.1",
     author="Edgar Ortiz",
     author_email="ed.ortizm@gmail.com",
