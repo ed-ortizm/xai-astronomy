2bd6a1052858ef779d08cf8481a4abd22247b20f
Edgar Ortiz <ed.ortizm@gmail.com>
2022-02-13 12:06:31 -0300

    Create modules with silly regressors to ilusstrate lime image explainer

--- src/astroExplain/toyRegressors.py
+++ src/astroExplain/toyRegressors.py
@@ -1,5 +1,31 @@
 import numpy as np
-class ToyModel:
+###############################################################################
+class GalaxyPlus:
+    """
+    Class to add all pixel values in an image
+    """
+
+    def __init__(self):
+        pass
+
+    def predict(self, image: np.array) -> np.array:
+
+        image = self._update_dimension(image)
+
+        # predict and normalize
+        prediction = np.sum(image, axis=(1, 2, 3))  # / image[0, :].size
+        # print(prediction.shape)
+
+        return prediction.reshape((-1, 1))
+
+    def _update_dimension(self, image: np.array) -> np.array:
+
+        if image.ndim == 3:
+            return image[np.newaxis, ...]
+
+        return image
+###############################################################################
+class CubePlus:
 
     def __init__(self,
         wave,
