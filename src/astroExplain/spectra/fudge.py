b500c101b9c671946ffc7f84765e3a7c8ee7f611
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-06-14 14:09:09 -0400

    feature: fudge with mean value per segment per channe
    With Fudge.with_mean, the fudged spectrum will have in each segment its mean value plus either the same noise present in that segment or the white noise. White noise can be set to zero

--- src/astroExplain/spectra/fudge.py
+++ src/astroExplain/spectra/fudge.py
@@ -21,7 +21,8 @@ class Fudge:
         INPUTS
 
         kernel_size: size of gaussian kernel used to smoot the spectrum.
-        sigma: standard deviation of white noise
+        sigma: standard deviation of white noise. If sigma is zero, it
+            returns the sahpe of the spectrum
 
         OUTPUT
         fudged_spectrum: filtered spectrum plus white noise
@@ -34,10 +35,50 @@ class Fudge:
 
         return fudged_spectrum
 
+    def with_mean(self, same_noise: bool=True, sigma: float=0) -> np.array:
+        """
+        Fudge spectrum with mean value per channel per segmment
+        and either same noise in original segment or white noise
+
+        INPUT
+
+        same_noise: if True, add the spectrum's noise to the mean value
+            of each segment. Otherwise add white noise, according to
+            sigma. If sigma is zero, there is no noise.
+        sigma: standard deviation of white noise if same_noise is False
+
+        OUTPUT
+        fudged_spectrum: original image + gaussian noise according
+            to mu and std parameters
+        """
+
+        fudged_spectrum = self.spectrum.copy()
+
+        for segment_id in np.unique(self.segments):
+
+            mask_segments = self.segments == segment_id
+
+            mean_per_segment_per_channel = np.mean(
+                self.spectrum[mask_segments], axis=(0, 1)
+            )
+
+            fudged_spectrum[mask_segments] = mean_per_segment_per_channel
+
+        if same_noise is True:
+
+            _, fudge_noise = self.filtered_spectrum(kernel_size)
+
+        else:
+
+            fudge_noise = self._white_noise(sigma)
+
+        fudged_spectrum += fudge_noise
+
+        return fudged_spectrum
 
     def flat(self,
         continuum: float=1.,
-        noise: str='spectrum',
+        same_noise: bool=True,
         kernel_size: int=3,
         sigma: float=1
     )-> np.array:
@@ -49,17 +90,15 @@ class Fudge:
         INPUTS
 
         continuum: value of the continuum
-        noise: options are {'spectrum', 'flat', 'white'}
-            'spectrum': add the spectrum's noise to the continuum
-            'flat': there is no noise added to the continuum
-            'user': add whithe noise with mu=0 and sigma as passed
-                in the arguments of this function
+        same_noise: if True, add the spectrum's noise to the continuum.
+            otherwise add white noise to the continuum, according
+            to sigma. If sigma is zero, there is no noise in the
+            flat continuum
         kernel_size: size of gaussian kernel used to smoot the spectrum.
             Necessary when implementin noise='spectrum'. The noise
             will be the original image minus the the filtered image
             with the gaussian kernel
-        sigma: standard deviation of white noise if noise is set
-            to 'white'
+        sigma: standard deviation of white noise if same_noise is False
 
         OUTPUT
 
@@ -67,22 +106,15 @@ class Fudge:
 
         """
 
-        if noise == "spectrum":
+        if same_noise is True:
 
             _, fudge_noise = self.filter_noise(kernel_size)
             fudged_spectrum = continuum + fudge_noise
 
-        elif noise == "flat":
-
-            fudged_spectrum = continuum * np.ones(self.spectrum.shape)
-
-        elif noise == "white":
+        else:
 
             fudge_noise = self._white_noise(sigma=sigma)
             fudged_spectrum = continuum + fudge_noise
-
-        else:
-            raise ValueError( "'noise' must be: 'spectrum', 'flat' or 'white'")
 
         return fudged_spectrum
 
@@ -205,24 +237,3 @@ class Fudge:
         return centroids
 
     ###########################################################################
-    def fudge_with_mean(self) -> np.array:
-        """
-        Fudge image with mean value per channel per segmment
-
-        OUTPUT
-        image_fudged: original image + gaussian noise according
-            to mu and std parameters
-        """
-        image_fudged = self.spectrum.copy()
-
-        for segment_id in np.unique(self.segments):
-
-            mask_segments = self.segments == segment_id
-
-            mean_per_segment_per_channel = np.mean(
-                self.spectrum[mask_segments], axis=(0, 1)
-            )
-
-            image_fudged[mask_segments] = mean_per_segment_per_channel
-
-        return image_fudged
