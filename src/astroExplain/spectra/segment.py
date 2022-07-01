c3b344098aa66317feba62400e46ac65c530065f
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-07-01 14:27:30 -0400

    feature: implement skimage.segmentation.slic for spectra

--- src/astroExplain/spectra/segment.py
+++ src/astroExplain/spectra/segment.py
@@ -1,19 +1,55 @@
+"""Segmentation algorithms for spectra"""
 import numpy as np
 from skimage.color import gray2rgb
-
+from skimage.segmentation import slic
 ###############################################################################
 class SpectraSegmentation:
     """
     Class with different segmentation algorithms for spectra
     """
 
-    ###########################################################################
     def __init__(self):
         pass
 
-    ###########################################################################
+    def kmeans(self,
+        spectra: np.array,
+        number_segments: int = 64,
+        sigma: float=3.,
+        compactness: float=0.01
+    ) -> np.array:
+        """
+        Segment spectra according to flux distribution [color] using
+        kmeans with algorithm skimage.segmentation.slic
+
+        INPUTS
+            spectra: array with a single spectrum
+            number_segments: define the width of each segment
+            sigma: shape of gaussian kernel to smooth spectrum
+            compacness: smaller values make clusters according to
+                flux values, while larger values make the segments
+                similar to equally sized segments
+
+        OUTPUTS
+            segments: array with integers representing each segment
+        """
+
+        # if spectra.ndim == 1 --> (1, flux, 3)
+        # if spectra.ndim == 2 --> (1, flux, 3)
+        # if spectra.ndim == 3 --> spectra
+        spectra = self.spectra_to_RGB(spectra)
+        spectra = spectra[..., 0].copy()
+
+        segments = slic(
+            spectra,
+            n_segments=number_segments,
+            sigma=sigma,
+            compactness=compactness
+        )
+
+        return segments.astype(int)
+
     def uniform(
-        self, spectra: np.array, number_segments: int = 100
+        self, spectra: np.array, number_segments: int = 64
     ) -> np.array:
         """
         Divide spectra in bins of equal size
@@ -50,7 +86,12 @@ class SpectraSegmentation:
         return segments.astype(int)
 
     ###########################################################################
-    def spectra_to_RGB(self, spectra):
+    @staticmethod
+    def spectra_to_RGB(spectra: np.array) -> np.array:
+        """
+        Convert spectra to RGB image, the height is set to 1 and
+        each channel is a copy of the spectrum
+        """
 
         # If a single spectrum is passed
         if spectra.ndim == 1:
@@ -67,5 +108,7 @@ class SpectraSegmentation:
         # if already image
         if spectra.ndim == 3:
             return spectra
+
+        raise ValueError("Input array shape does not represent a spectrum")
 
     ###########################################################################
