155636774ae16a0af162a235080f211c0e0f2548
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-11-24 11:08:53 -0300

    chore: update parallel explanation script

--- spectra/parallel_explain.py
+++ spectra/parallel_explain.py
@@ -35,20 +35,20 @@ from astroExplain.spectra.utils import get_anomaly_score_name
 if __name__ == "__main__":
 
     mp.set_start_method("spawn", force=True)
-    ###########################################################################
+
     start_time = time.time()
-    ###########################################################################
+
     parser = ConfigParser(interpolation=ExtendedInterpolation())
     config_file_name = "parallel_explain.ini"
     parser.read(f"{config_file_name}")
-    ###########################################################################
+
     # Check files and directory
     check = FileDirectory()
     # Handle configuration file
     configuration = ConfigurationFile()
-    ###########################################################################
+
     counter = mp.Value("i", 0)
-    ###########################################################################
+
     # strings to get right paths to data
     explanation_directory = parser.get("directory", "explanation")
     spectra_name = parser.get("file", "spectra")
@@ -71,8 +71,8 @@ if __name__ == "__main__":
         score_name = f"{metric}"
 
     spectra_to_explain = np.load(
-        # f"{explanation_directory}/{score_name}/{spectra_name}"
-        f"{explanation_directory}/{spectra_name}"
+        f"{explanation_directory}/{score_name}/{spectra_name}"
+        # f"{explanation_directory}/{spectra_name}"
     )
 
     if spectra_to_explain.ndim == 1:
@@ -85,20 +85,19 @@ if __name__ == "__main__":
         spectra_to_explain.reshape(-1),
     )
 
-    ###########################################################################
     meta_data_spectra_name = parser.get("file", "meta")
     meta_data_directory = parser.get("directory", "meta")
 
     meta_data_spectra_df = pd.read_csv(
-        # f"{explanation_directory}/{score_name}/{meta_data_spectra_name}",
-        f"{explanation_directory}/{meta_data_spectra_name}",
+        f"{explanation_directory}/{score_name}/{meta_data_spectra_name}",
+        # f"{explanation_directory}/{meta_data_spectra_name}",
         index_col="specobjid",
     )
-    ###########################################################################
+
     wave_name = parser.get("file", "grid")
     wave = np.load(f"{meta_data_directory}/{wave_name}")
     wave = RawArray(np.ctypeslib.as_ctypes_type(wave.dtype), wave)
-    ###########################################################################
+
     model_id = parser.get("file", "model_id")
     model_directory = parser.get("directory", "model")
     model_directory = f"{model_directory}/{model_id}"
@@ -109,14 +108,13 @@ if __name__ == "__main__":
         np.ctypeslib.as_ctypes_type(specobjid.dtype), specobjid.reshape(-1)
     )
 
-    ###########################################################################
     print("Load score and lime configurations", end="\n")
 
     score_parser = ConfigParser(interpolation=ExtendedInterpolation())
     score_parser_name = parser.get("score", "configuration")
     score_parser.read(
-        # f"{explanation_directory}/{score_name}/{score_parser_name}"
-        f"{explanation_directory}/{score_parser_name}"
+        f"{explanation_directory}/{score_name}/{score_parser_name}"
+        # f"{explanation_directory}/{score_parser_name}"
     )
     score_config = score_parser.items("score")
     score_config = configuration.section_to_dictionary(
@@ -133,7 +131,6 @@ if __name__ == "__main__":
         score_configuration["epsilon"] = score_config["epsilon"]
         score_configuration["relative"] = relative
         score_configuration["percentage"] = percentage
-    ###########################################################################
 
     lime_configuration = parser.items("lime")
     lime_configuration = configuration.section_to_dictionary(
@@ -143,7 +140,7 @@ if __name__ == "__main__":
     fudge_configuration = configuration.section_to_dictionary(
         parser.items("fudge"), value_separators=[]
     )
-    ###########################################################################
+
     save_explanation_to = (
         f"{explanation_directory}/{score_name}/"
         # f"xai_{spectra_name.split('.')[0]}"
@@ -151,6 +148,8 @@ if __name__ == "__main__":
     check.check_directory(save_explanation_to, exit_program=False)
 
     explanation_runs = glob.glob(f"{save_explanation_to}/*/")
+
+    print(explanation_runs, save_explanation_to)
 
     if len(explanation_runs) == 0:
 
@@ -163,7 +162,6 @@ if __name__ == "__main__":
 
     save_explanation_to = f"{save_explanation_to}/{run}"
     check.check_directory(f"{save_explanation_to}", exit_program=False)
-    ###########################################################################
     number_processes = parser.getint("configuration", "jobs")
     cores_per_worker = parser.getint("configuration", "cores_per_worker")
 
@@ -190,12 +188,10 @@ if __name__ == "__main__":
             np.arange(spectra_to_explain_shape[0]),
         )
 
-    ###########################################################################
     with open(
         f"{save_explanation_to}/{config_file_name}", "w", encoding="utf8"
     ) as config_file:
 
         parser.write(config_file)
-    ###########################################################################
     finish_time = time.time()
     print(f"\nRun time: {finish_time - start_time:.2f}")
